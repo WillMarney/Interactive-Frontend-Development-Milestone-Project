a4840fd8bfc2de31c6befe394f0418d72f855140
WillMarney <will.marney@hotmail.co.uk>
2020-03-23 09:44:57 +0000

    pages added, cards created

---
+++ assets/javascript.js
@@ -0,0 +1,52 @@
+// Cards
+
+const cardsArray = [
+  {
+    name: 'bloodhound',
+    img: 'images/',
+  },
+  {
+    name: 'gibraltar',
+    img: 'images/',
+  },
+  {
+    name: 'lifeline',
+    img: 'images/',
+  },
+  {
+    name: 'pathfinder',
+    img: 'images/',
+  },
+  {
+    name: 'wraith',
+    img: 'images/',
+  },
+  {
+    name: 'bangalore',
+    img: 'images/',
+  },
+  {
+    name: 'caustic',
+    img: 'images/',
+  },
+  {
+    name: 'mirage',
+    img: 'images/',
+  },
+  {
+    name: 'octane',
+    img: 'images/',
+  },
+  {
+    name: 'wattson',
+    img: 'images/',
+  },
+  {
+    name: 'crypto',
+    img: 'images/',
+  },
+  {
+    name: 'revenant',
+    img: 'images/',
+  },
+]
